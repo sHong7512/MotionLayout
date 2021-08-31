d51ae31fe7ec3d9a27450c334ec4585e201d4631
SoonHong Kwon <ksh7512@uangel.com>
2021-08-31 17:33:51 +0900

    Add Login

--- app/src/main/java/com/shong/motionlayout/SplashActivity.kt
+++ app/src/main/java/com/shong/motionlayout/SplashActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.constraintlayout.motion.widget.MotionLayout
+import kotlinx.coroutines.*
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,22 +12,33 @@ class SplashActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash)
 
         val motionLayout = findViewById<MotionLayout>(R.id.motionLayout)
+        var isOk = false
         motionLayout.addTransitionListener(object : MotionLayout.TransitionListener {
             override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
-
             }
 
             override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {
             }
 
             override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
-                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
-                finish()
+                isOk = true
             }
 
             override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {
             }
 
         })
+
+        CoroutineScope(Dispatchers.Main).launch {
+            while (true) {
+                if (!isOk) delay(500L)
+                else break
+            }
+//            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+//            finish()
+            motionLayout.transitionToState(R.id.startLogin)
+            motionLayout.transitionToStart()
+        }
+
     }
 }
